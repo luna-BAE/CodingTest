db79f84b4ee3462650a126b56f255eff104881da
luna <dichotomy.bgm@gmail.com>
2021-09-29 22:14:43 +0900

    set up 'NumberBlock'

---
+++ numberBlock/src/numberBlock/NmuberBlockVersion1.java
@@ -0,0 +1,21 @@
+package numberBlock;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class NmuberBlockVersion1 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+	
+		String input = read.readLine();
+		
+		int[] temp;
+		int[] storage;
+		
+		
+		
+	}
+	
+}
