fdb1af39e7c15cee46c5b14e08d4ee4b324adf80
luna <dichotomy.bgm@gmail.com>
2021-09-30 12:32:15 +0900

    set up testcode

---
+++ numberFlip/src/numberFlip/NumberFlipVersion1.java
@@ -0,0 +1,20 @@
+package numberFlip;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class NumberFlipVersion1 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = read.readLine();
+		
+		for ( int no = input.length()-1; no >= 0; no-- ) {
+			System.out.print(input.charAt(no));
+		}
+		
+	}
+	
+}
---
+++ scoring/src/scoring/ScoringVersion1.java
@@ -0,0 +1,18 @@
+package scoring;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ScoringVersion1 {
+
+	public static void main(String[] args) throws Exception {
+		
+		BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = read.readLine();
+		
+		
+		
+	}
+	
+}
